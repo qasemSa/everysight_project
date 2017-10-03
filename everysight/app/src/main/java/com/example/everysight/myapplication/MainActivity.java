81e61498310482d252d3f5ca7a0d445dbdd0e80c
qasemSa <sqasem@campus.technion.ac.il>
2017-10-03 13:13:46 +0300

    first commit

--- everysight/app/src/main/java/com/example/everysight/myapplication/MainActivity.java
+++ everysight/app/src/main/java/com/example/everysight/myapplication/MainActivity.java
@@ -15,7 +15,9 @@
 
 package com.example.everysight.myapplication;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -24,35 +26,29 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.opengl.Matrix;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.util.Log;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.everysight.activities.managers.EvsPopupManager;
 import com.everysight.activities.managers.EvsServiceInterfaceManager;
 import com.everysight.base.EvsContext;
 import com.everysight.carousel.EvsCarouselActivity;
-import com.everysight.common.carouselm.CarouselBehavior;
-import com.everysight.common.carouselm.ItemInfo;
 import com.everysight.environment.EvsConsts;
 import com.everysight.notifications.EvsAlertNotification;
 import com.everysight.notifications.EvsNotification;
-import com.everysight.notifications.EvsToast;
 import com.everysight.utilities.SensorOrientationUtils;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import com.example.everysight.myapplication.R;
 
 /*
 This is a standard Android Location service example. No difference what-so-ever.
@@ -60,8 +56,25 @@ The only thing you should keep in mind is the source of the GPS data.
 The source depends on Glasses global system configuration.
 It can be either from on-board glasses GPS or paired phone location service.
 */
-public class MainActivity extends EvsCarouselActivity implements LocationListener
+public class MainActivity extends EvsCarouselActivity implements LocationListener,SensorEventListener
 {
+
+    private SurfaceView surfaceView;
+    private FrameLayout cameraContainerLayout;
+    private AROverlayView arOverlayView;
+    private ARCamera arCamera;
+
+    private final static int REQUEST_CAMERA_PERMISSIONS_CODE = 11;
+    public static final int REQUEST_LOCATION_PERMISSIONS_CODE = 0;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 10 meters
+    private static final long MIN_TIME_BW_UPDATES = 0;//1000 * 60 * 1; // 1 minute
+
+    public Location mlocation;
+    boolean isGPSEnabled;
+    boolean isNetworkEnabled;
+    boolean locationServiceAvailable;
+
     private final String TAG = "MainActivity";
     private TextView mCxtCenterLable = null;
     private TextView mMenuLable = null;
@@ -77,37 +90,9 @@ public class MainActivity extends EvsCarouselActivity implements LocationListene
     private Sensor mQuaternion;
     private DecimalFormat mFormat;
     private float[] mLosAngles = null;
-    private HandlerThread mSensorThread;
-    private Handler mSensorHandler;
-    private SensorEventListener mSensorListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event)
-        {
-            if (event == null)
-            {
-                return;
-            }
 
-            int sensor_type = event.sensor.getType();
-            if(sensor_type != Sensor.TYPE_ROTATION_VECTOR)
-            {
-                return;
-            }
-
-            float[] quaternion = event.values.clone();//Quarernion is [x,y,z,w]
-            mLosAngles = SensorOrientationUtils.QuaternionToAngles(quaternion);
-        }
-        @Override
-        public void onAccuracyChanged(Sensor var1, int var2){
-            return;
-        }
-
-    };;
-
-
-    private void save_point(double x,double y,double z,double yaw,double pitch,double roll){
-        String point = Double.toString(x) + "," + Double.toString(y)+","+Double.toString(z)+","+Double.toString(yaw)+","+Double.toString(pitch)+","+Double.toString(roll)+"\n";
-        byte[] data = point.getBytes();
+    private void save_text(String str){
+        byte[] data = str.getBytes();
         if (data == null)
         {
             return;
@@ -128,7 +113,10 @@ public class MainActivity extends EvsCarouselActivity implements LocationListene
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+    }
+    private void save_point(double x,double y,double z,double yaw,double pitch,double roll){
+        String point = Double.toString(x) + "," + Double.toString(y)+","+Double.toString(z)+","+Double.toString(yaw)+","+Double.toString(pitch)+","+Double.toString(roll)+"\n";
+        save_text(point);
     }
     /******************************************************************/
     @Override
@@ -157,19 +145,25 @@ public class MainActivity extends EvsCarouselActivity implements LocationListene
                 mPopupManager = popupManager;
             }
         });
-
+        cameraContainerLayout = (FrameLayout) findViewById(R.id.camera_container_layout);
+        surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        arOverlayView = new AROverlayView(this);
     }
 
     @Override
     protected void onResume()
     {
         super.onResume();
+        requestLocationPermission();
+        requestCameraPermission();
+        initAROverlayView();
     }
 
     @Override
     protected void onPause()
     {
         super.onPause();
+
     }
 
 
@@ -200,16 +194,22 @@ public class MainActivity extends EvsCarouselActivity implements LocationListene
 
         // register for updates - get GPS point as soon as it is available
         mLocationManager.requestLocationUpdates(provider, 0, 0, this);
+        if (mLocationManager != null)  {
+            mlocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            updateLatestLocation();
+        }else{
+            Location Xlocation = new Location("XARPoint");
+            Xlocation.setLatitude(10);
+            Xlocation.setLongitude(10);
+            Xlocation.setAltitude(10);
+            mlocation = Xlocation;
+            updateLatestLocation();
+        }
 
         //init los manager
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mQuaternion = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        //mFormat = new DecimalFormat("#.##");
-        //mSensorManager.registerListener(mSensorListener, mQuaternion, SensorManager.SENSOR_DELAY_FASTEST);
-        mSensorThread = new HandlerThread("Sensor thread", Thread.MAX_PRIORITY);
-        mSensorThread.start();
-        mSensorHandler = new Handler(mSensorThread.getLooper()); //Blocks until looper is prepared, which is fairly quick
-        mSensorManager.registerListener(mSensorListener, mQuaternion, SensorManager.SENSOR_DELAY_FASTEST, mSensorHandler);
+        mSensorManager.registerListener(this, mQuaternion, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     /******************************************************************/
@@ -231,6 +231,12 @@ public class MainActivity extends EvsCarouselActivity implements LocationListene
     }
 
     /******************************************************************/
+    private void updateLatestLocation() {
+        if (arOverlayView !=null) {
+            arOverlayView.updateCurrentLocation(mlocation);
+        }
+    }
+
     @Override
     public void onLocationChanged(Location location)
     {
@@ -245,6 +251,8 @@ public class MainActivity extends EvsCarouselActivity implements LocationListene
         save_point(x,y,z,Math.toDegrees(losAngles[0]),Math.toDegrees(losAngles[1]),Math.toDegrees(losAngles[2]));
         mCxtCenterLable.setText(location.getTime() + "\n" + x + "\n" + y);
         Log.e(TAG, "got new location !");
+        mlocation = location;
+        updateLatestLocation();
     }
 
     /******************************************************************/
@@ -272,7 +280,7 @@ public class MainActivity extends EvsCarouselActivity implements LocationListene
     public void onTap()
     {
         if(first_launch) {
-            File data_file = new File(EvsConsts.EVS_DIR, "data.txt");
+           File data_file = new File(EvsConsts.EVS_DIR, "data.txt");
             if(data_file.exists()){
                 data_file.delete();
                 try {
@@ -281,12 +289,21 @@ public class MainActivity extends EvsCarouselActivity implements LocationListene
                     e.printStackTrace();
                 }
             }
+            save_text("GPS data\n");
             initGps();
+            mMenuLable.setText("started GPS data collecting!");
         }else{
-            mLocationManager.removeUpdates(this);
-            mSensorManager.unregisterListener(mSensorListener);
-            mQuaternion = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-            mSensorThread.quitSafely();
+            save_text("finished collecting GPS data\n");
+            mMenuLable.setText("Finished GPS data collecting!");
+            if(mLocationManager != null)
+            {
+                mLocationManager.removeUpdates(this);
+            }
+            if(mSensorManager != null) {
+                mSensorManager.unregisterListener(this);
+                mQuaternion = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+                mLosAngles = null;
+            }
             cordinate_pointer = 0;
         }
         first_launch = !first_launch;
@@ -306,4 +323,97 @@ public class MainActivity extends EvsCarouselActivity implements LocationListene
             mPopupManager.notify(notif);
         }
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event == null)
+        {
+            return;
+        }
+
+        int sensor_type = event.sensor.getType();
+        if(sensor_type != Sensor.TYPE_ROTATION_VECTOR)
+        {
+            return;
+        }
+
+        float[] quaternion = event.values.clone();//Quarernion is [x,y,z,w]
+        mLosAngles = SensorOrientationUtils.QuaternionToAngles(quaternion);
+
+
+        float[] rotationMatrixFromVector = new float[16];
+        float[] projectionMatrix = new float[16];
+        float[] rotatedProjectionMatrix = new float[16];
+
+        SensorManager.getRotationMatrixFromVector(rotationMatrixFromVector, event.values);
+        if (arCamera != null) {
+            projectionMatrix = arCamera.getProjectionMatrix();
+
+        }
+        float ratio = (float) arOverlayView.getWidth() / arOverlayView.getHeight();
+        final int OFFSET = 0;
+        final float LEFT =  -ratio;
+        final float RIGHT = ratio;
+        final float BOTTOM = -1;
+        final float TOP = 1;
+        Matrix.frustumM(projectionMatrix, OFFSET, LEFT, RIGHT, BOTTOM, TOP, 0.5f, 2000);
+        Matrix.multiplyMM(rotatedProjectionMatrix, 0, projectionMatrix, 0, rotationMatrixFromVector, 0);
+        this.arOverlayView.updateRotatedProjectionMatrix(rotatedProjectionMatrix);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        return;
+    }
+
+
+    public void requestCameraPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                this.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            this.requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSIONS_CODE);
+        } else {
+            initARCameraView();
+        }
+    }
+
+    public void requestLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSIONS_CODE);
+        }
+    }
+
+    public void initAROverlayView() {
+        if (arOverlayView.getParent() != null) {
+            ((ViewGroup) arOverlayView.getParent()).removeView(arOverlayView);
+        }
+        cameraContainerLayout.addView(arOverlayView);
+    }
+
+    public void initARCameraView() {
+        reloadSurfaceView();
+
+        if (arCamera == null) {
+            arCamera = new ARCamera(this, surfaceView);
+        }
+        if (arCamera.getParent() != null) {
+            ((ViewGroup) arCamera.getParent()).removeView(arCamera);
+        }
+        cameraContainerLayout.addView(arCamera);
+        arCamera.setKeepScreenOn(true);
+
+    }
+
+
+
+    private void reloadSurfaceView() {
+        if (surfaceView.getParent() != null) {
+            ((ViewGroup) surfaceView.getParent()).removeView(surfaceView);
+        }
+
+        cameraContainerLayout.addView(surfaceView);
+    }
+
+
 }
